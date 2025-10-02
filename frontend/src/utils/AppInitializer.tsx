65dca5513b3db110ef0fd62483439a9957179754
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2025-10-02 15:37:01 +0200

    cleaning up comments

--- frontend/src/utils/AppInitializer.tsx
+++ frontend/src/utils/AppInitializer.tsx
@@ -37,66 +37,6 @@ const AppInitializer: React.FC<Props> = ({ children }) => {
 
 export default AppInitializer;
 
-// import { useGetCurrentUserQuery } from "../redux/apiSlice";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { setCredentials, logout, setInitialized } from "../redux/authSlice";
-// import type { AppDispatch } from "../redux/store";
-
-// interface Props {
-//   children: React.ReactNode;
-// }
-
-// const AppInitializer: React.FC<Props> = ({ children }) => {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const { data, error, isLoading } = useGetCurrentUserQuery(undefined);
-
-//   useEffect(() => {
-//     if (data) {
-//       dispatch(setCredentials(data));
-//     } else if (error && "status" in error && error.status === 401) {
-//       dispatch(logout());
-//     }
-//     dispatch(setInitialized(true));
-//   }, [data, error, dispatch]);
-
-//   if (isLoading) return <div>Loading...</div>;
-
-//   return <>{children}</>;
-// };
-
-// export default AppInitializer;
-// AppInitializer.tsx
-// import { useGetCurrentUserQuery } from "../redux/apiSlice";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { setCredentials, logout, setInitialized } from "../redux/authSlice";
-// import type { AppDispatch } from "../redux/store";
-
-// interface Props {
-//   children: React.ReactNode;
-// }
-
-// const AppInitializer: React.FC<Props> = ({ children }) => {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const { data, error, isLoading } = useGetCurrentUserQuery(undefined);
-
-//   useEffect(() => {
-//     if (data) {
-//       dispatch(setCredentials(data));
-//     } else if (error && "status" in error && error.status === 401) {
-//       dispatch(logout());
-//     }
-//     dispatch(setInitialized(true));
-//   }, [data, error, dispatch]);
-//     if (isLoading) return <div>Loading...</div>;
-
-//   // Не блокуємо рендер дітей під час завантаження
-//   // Повертаємо children навіть під час isLoading
-//   return <>{children}</>;
-// };
-
-// export default AppInitializer;
 
 
 
